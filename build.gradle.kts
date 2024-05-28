61e83f0d70e953cc0006affc68d4ce74f3f76a3a
Adrien Pessu <7055334+adrienpessu@users.noreply.github.com>
2024-05-28 19:39:02 +0200

    remove kover because it's not compatible with Kotlin 2.0 yet

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,13 +58,13 @@ qodana {
 }
 
 // Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
-koverReport {
-    defaults {
-        xml {
-            onCheck = true
-        }
-    }
-}
+//koverReport {
+//    defaults {
+//        xml {
+//            onCheck = true
+//        }
+//    }
+//}
 
 tasks {
     wrapper {
