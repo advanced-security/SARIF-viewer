0cd2589e99389315eceb1e6f99ab0f4672426958
Adrien Pessu <7055334+adrienpessu@users.noreply.github.com>
2024-06-12 13:50:37 +0200

    upgrade Kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,13 +58,15 @@ qodana {
 }
 
 // Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
-//koverReport {
-//    defaults {
-//        xml {
-//            onCheck = true
-//        }
-//    }
-//}
+kover {
+    reports {
+        total {
+            xml {
+                onCheck = true
+            }
+        }
+    }
+}
 
 tasks {
     wrapper {
