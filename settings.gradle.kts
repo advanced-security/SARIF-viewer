c870fd36a67753f614e79cb401b89c5fd16d9df4
Adrien Pessu <7055334+adrienpessu@users.noreply.github.com>
2025-12-30 15:49:24 +0100

    upgrade Gradle and plugin dependencies; refactor build configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,22 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://plugins.jetbrains.com/maven")
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://www.jetbrains.com/intellij-repository/snapshots")
+    }
+}
+
 rootProject.name = "SARIF-viewer"
 
 dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://www.jetbrains.com/intellij-repository/snapshots")
+        maven("https://plugins.jetbrains.com/maven")
+    }
     versionCatalogs {
         create("viewer") {
             library("java-sarif", "com.contrastsecurity:java-sarif:2.0")
